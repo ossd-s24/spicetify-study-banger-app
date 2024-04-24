189023fa1989a915ba5a5afaa6c39e97aefce8f2
b-chen00 <bc2982@nyu.edu>
2024-04-23 20:44:13 -0400

    add starrynight theme change functionality

--- src/StudyTimePage.tsx
+++ src/StudyTimePage.tsx
@@ -5,12 +5,15 @@ interface Props {
     onEndStudy: () => void;
 }
 
-const StudyTimePage: React.FC<Props> = ({onEndStudy}) => {
+const StudyTimePage: React.FC<Props> = ({onEndStudy, onChangeTheme}) => {
     return (
         <>
             <button className={`${styles.button} ${styles.border_green}`} onClick={onEndStudy}>
-                    <span className={`${styles.circle} ${styles.green}`} />{"Study on"}
-                </button>
+                <span className={`${styles.circle} ${styles.green}`} />{"Study on"}
+            </button>
+            <button className={`${styles.button} ${styles.border_green}`} onClick={onChangeTheme}>
+                <span className={`${styles.circle} ${styles.green}`} />{"Change theme"}
+            </button>
             <div className={styles.title}>{"Study Time!"}</div>
         </>
     );
